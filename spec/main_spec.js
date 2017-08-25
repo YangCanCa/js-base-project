0f2b2b98135a26fdab8904f9f7ce034172cf8c84
YangCanCa <yangcanca@example.com>
2017-08-25 12:14:08 +0000

    first commit

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -9,23 +9,58 @@ chai.use(sinonChai);
 var main = require("../lib/main.js");
 
 
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
+describe("return barcode given ZIPcode ", function(){
 
-    it("测试用例1", function(){
 
-        var result = main();
+    it("return null string given null barcode", function(){
+
+        var result = main('');
         var expect_string = '';
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("return 5 digit number given barcodes ", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
+       var result= main('|:::||:::||::||::|::|:|:|:|');
+     var expect_string = '11345';
+      
         expect(expect_string).to.equal(result);
+    });
+     it("return 10 digit number given barcode", function () {
+        var result = main('|:::||::|:|::|:|:|::|:|:|:||::::||::|:|::|:|::|');
+        var expect_string = '12245-0699';
+
+        expect(result).to.equal(expect_string);
+    });
+});
+describe("return barcode given ZIP code", function(){
+    //sinon.spy(console,'log');
+    it("return null given null string", function(){
+        var result = main('');
+        var expect_string = '';
+        
+        expect(result).to.equal(expect_string);
+    });
+
+    it("return barcode given 5 digit num", function(){
+        var result = main('12345');  //验证码为：5
+        var expect_string = '|:::||::|:|::||::|::|:|:|::|:|:|';
+        
+        expect(result).to.equal(expect_string);
+    });
+
+    it("return barcode given 9 digit num", function () {
+        var result = main('223450688');  //验证码为：2
+        var expect_string = '|::|:|::|:|::||::|::|:|:|:||::::||::|::|:|::|:::|:||';
+
+        expect(result).to.equal(expect_string);
+    });
+
+    it("return barcode given 10 digit num", function () {
+        var result = main('12345-0678'); //验证码为：4
+        var expect_string = '|:::||::|:|::||::|::|:|:|:||::::||::|:::||::|::|::||';
+
+        expect(result).to.equal(expect_string);
     });
 });
